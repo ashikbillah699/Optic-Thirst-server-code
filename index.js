7263bef4f2e5562408c595a06a02fd06a300078e
Md Ashik Billah <ashikbillah699@gmail.com>
2022-11-09 16:30:48 +0600

    mongodb,dotenv,express install

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-var cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rhwxyri.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
